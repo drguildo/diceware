0fc385db37f35be8c3ae37bda76f65a9f1f64a8d
Simon Morgan <sjm@sjm.io>
2021-10-02 10:31:33 +0100

    Bump dependency versions

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ fn main() {
             println!("wordlist length: {}", wordlist.len());
 
             let numwords = matches.value_of("numwords").unwrap().parse::<u8>().unwrap();
-        
+
             println!("{}", generate_password(&wordlist, numwords));
         }
         Err(e) => {
@@ -50,7 +50,7 @@ fn generate_password(wordlist: &Vec<String>, num_words: u8) -> String {
     let mut rng = rand::thread_rng();
     let mut password_words: Vec<String> = vec![];
     for _ in 0..num_words {
-        let random_word_index = rng.gen_range(0, wordlist.len());
+        let random_word_index = rng.gen_range(0..wordlist.len());
         let random_word = wordlist.get(random_word_index).unwrap();
         password_words.push(random_word.clone());
     }
