43334417847cb3ae31d4d11806074c4b5143ab65
Simon Morgan <sjm@sjm.io>
2026-04-15 18:29:40 +0100

    Bump

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 pub fn generate_password(wordlist: &Vec<String>, num_words: u8) -> String {
     use rand::Rng;
 
-    let mut rng = rand::thread_rng();
+    let mut rng = rand::rng();
     let mut password_words: Vec<String> = vec![];
     for _ in 0..num_words {
-        let random_word_index = rng.gen_range(0..wordlist.len());
+        let random_word_index = rng.random_range(0..wordlist.len());
         let random_word = wordlist.get(random_word_index).unwrap();
         password_words.push(random_word.clone());
     }
